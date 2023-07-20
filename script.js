7bdb93a5f3badeb0ca635144afd12ed55bd676b1
umesh jyani <49265224+Umeshjyani@users.noreply.github.com>
2023-07-20 20:05:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function nonrepating(str) {
+	const charF=[];
+	for(const char of str){
+		charF[char]=(charF[char] || 0)+ 1;
+	}
+	for(const char of str){
+		if(charF[char]==1)
+			return char;
+	}
+	return null;
+}
+let str=prompt("Enter a string");   
+alert(nonrepating(str));
